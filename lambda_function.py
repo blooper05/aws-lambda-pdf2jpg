7263ef516e50611cea3e1cfb2c582fe8d4978f18
blooper05 <legend.of.blooper@gmail.com>
2017-06-22 23:24:08 +0700

    :tada: Implement simple pdf to jpg converter

---
+++ lambda_function.py
@@ -0,0 +1,17 @@
+import boto3
+from wand.image import Image
+
+def lambda_handler(event, context):
+    bucket   = event['Records'][0]['s3']['bucket']['name']
+    filename = event['Records'][0]['s3']['object']['key']
+    tmp_path = '/tmp/' + filename
+    client   = boto3.client('s3')
+    client.download_file(bucket, filename, tmp_path)
+
+    with Image(filename = tmp_path) as img:
+        filename = filename.replace('.pdf', '.jpg')
+        tmp_path = '/tmp/' + filename
+        img.format = 'jpeg'
+        img.save(filename = tmp_path)
+
+    client.upload_file(tmp_path, bucket, filename)
