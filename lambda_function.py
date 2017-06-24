c9aa93b460e9678d8c06d3665eb5f09816d0b247
blooper05 <legend.of.blooper@gmail.com>
2017-06-24 14:15:25 +0700

    :+1: Add some options

--- lambda_function.py
+++ lambda_function.py
@@ -8,10 +8,14 @@ def lambda_handler(event, context):
     client   = boto3.client('s3')
     client.download_file(bucket, filename, tmp_path)
 
-    with Image(filename = tmp_path) as img:
+    with Image(filename = tmp_path, resolution = 600) as img:
         filename = filename.replace('.pdf', '.jpg')
         tmp_path = '/tmp/' + filename
-        img.format = 'jpeg'
+
+        img.format        = 'jpeg'
+        img.alpha_channel = False
+
+        img.transform(resize = '1920x')
         img.save(filename = tmp_path)
 
     client.upload_file(tmp_path, bucket, filename)
